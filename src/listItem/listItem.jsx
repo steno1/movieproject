9a69a1351685a807370d6b4015a2239fe3bb4d14
PRINCELEY <onuprinceley@gmail.com>
2023-01-07 07:34:16 +0100

    updated code on 7 jan 2023

--- src/listItem/listItem.jsx
+++ src/listItem/listItem.jsx
@@ -1,9 +1,55 @@
 import "./listItem.scss"
-const ListItem=()=>{
+import PlayArrowIcon from '@mui/icons-material/PlayArrow';
+import AddIcon from '@mui/icons-material/Add';
+import ThumbUpOutlinedIcon from '@mui/icons-material/ThumbUpOutlined';
+import ThumbDownAltOutlinedIcon from '@mui/icons-material/ThumbDownAltOutlined';
+import { useState } from "react";
+const ListItem=({index})=>{
+  const [isHovered, setIsHovered]=useState(false)
+  const trailer="https://media.istockphoto.com/id/1184888786/video/indian-farm.mp4?s=mp4-640x640-is&k=20&c=mNuGE9DEfImq5tYukcsxifFUNG0BwHCO7Q7MMZOZsYA=";
   return (
-    <div className="listItem">
-lisItem
+    <div className="listItem" style={{left:isHovered && index * 225-50+ index *2.5}}
+    onMouseEnter={()=>setIsHovered(true)} onMouseLeave={()=>setIsHovered(false)}>
+<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTCm92FzNUC-CCSVwkfFxvqtkwDxsznU93-uA&usqp=CAU" alt=""/>
+  {isHovered && (
+    <>
+    <video src={trailer} autoPlay={true} loop/>
+  
+  {/* listItem  general information */}  
+<div className="info">
+    <div className="icon">
+<PlayArrowIcon/>
+<AddIcon/>
+<ThumbUpOutlinedIcon/>
+<ThumbDownAltOutlinedIcon />
     </div>
+
+    <div className="itemInfoTop">
+<span>2hr 30 mins</span>
+<span className="limit">+18</span>
+<span>2003</span>
+    </div>  
+
+    <div className="desc">
+hello how are u today
+hello how are u today
+hello how are u today
+hello how are u today
+hello how are u today
+hello how are u today
+hello how are u today
+    </div>
+
+    <div className="genre">
+Action
+    </div>
+      </div>
+    
+    </>
+  )}
+  
+    </div>
+    
   ) 
 }
 export default ListItem
--- src/movieList/List.jsx
+++ src/movieList/List.jsx
@@ -8,8 +8,14 @@ const List=()=>{
     //make the slider not to overflow
 const [slideNumber, setSlideNumber]=useState(0);
 
+//make the arrow at left to appear when slider moves on click
+const [isMoved, setIsMoved]=useState(false);
+
+
     const listRef=useRef()//use in getting the container Div
     const handleClick=(direction)=>{
+        //when button is clicked setIsMoved=true
+        setIsMoved(true);
         let distance=listRef.current.getBoundingClientRect().x-50
         if(direction==="left" && slideNumber>0){
             setSlideNumber(slideNumber -1);
@@ -26,20 +32,21 @@ listRef.current.style.transform=`translateX(${230+distance}px)`
 <div className="wrapper">
 <ArrowBackIosNewOutlinedIcon className="sliderArrow left"
     onClick={()=>handleClick("left")}
+    style={{display:!isMoved && "none"}}//if slide doesnt move, hide arrow
 />
 <div className="container" ref={listRef}>
 {/* Each listItem in List contains a movie, 
 so each of the list component has 10 listItem, containing movies*/}
-<ListItem/>
-<ListItem/>
-<ListItem/>
-<ListItem/>
-<ListItem/>
-<ListItem/>
-<ListItem/>
-<ListItem/>
-<ListItem/>
-<ListItem/>
+<ListItem index={0}/>
+<ListItem index={1}/>
+<ListItem index={2}/>
+<ListItem index={3}/>
+<ListItem index={4}/>
+<ListItem index={5}/>
+<ListItem index={6}/>
+<ListItem index={7}/>
+<ListItem index={8}/>
+<ListItem index={9}/>
 </div>
 <ArrowForwardIosOutlinedIcon className="sliderArrow right"
     onClick={()=>handleClick("right")}
